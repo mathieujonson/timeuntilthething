1c5b7a41e1809691313fe7a725852403523199b1
Matthew Johnson <matthewjohnson@a-xrc7jjg5j.carfax.cfx>
2021-02-05 14:32:46 -0600

    refactors imports in shared stack

--- tutt-infrastructure/lib/tutt-shared-stack.ts
+++ tutt-infrastructure/lib/tutt-shared-stack.ts
@@ -1,50 +1,81 @@
-import cdk = require('@aws-cdk/core')
-import route53 = require('@aws-cdk/aws-route53')
-import s3 = require('@aws-cdk/aws-s3')
 import targets = require('@aws-cdk/aws-route53-targets')
-import apigateway = require('@aws-cdk/aws-apigateway')
+import { CloudFrontWebDistribution } from '@aws-cdk/aws-cloudfront'
+import {
+  ARecord,
+  CnameRecord,
+  HostedZone,
+  RecordTarget,
+} from '@aws-cdk/aws-route53'
+import { Bucket } from '@aws-cdk/aws-s3'
+import { Construct, Stack, StackProps } from '@aws-cdk/core'
 
-export class TuttSharedStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class TuttSharedStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    const zone = route53.HostedZone.fromLookup(this, 'tutt-hosted-zone', {
-      domainName: 'timeuntilthething.com'
+    const zone = HostedZone.fromLookup(this, 'tutt-hosted-zone', {
+      domainName: 'timeuntilthething.com',
     })
 
-    const siteBucket = new s3.Bucket(this, 'timeuntilthething.com', {
+    const siteBucket = new Bucket(this, 'timeuntilthething.com', {
       bucketName: 'timeuntilthething.com',
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: 'error.html',
-      publicReadAccess: true
+      publicReadAccess: true,
     })
 
-    const wwwBucket = new s3.Bucket(this, 'www.timeuntilthething.com', {
+    const wwwBucket = new Bucket(this, 'www.timeuntilthething.com', {
       bucketName: 'www.timeuntilthething.com',
-      websiteIndexDocument: 'index.html',
-      websiteRoutingRules: [
-        {
-          hostName: 'timeuntilthething.com',
-          httpRedirectCode: '302',
-          protocol: s3.RedirectProtocol.HTTP
-        }
-      ]
+      websiteRedirect: {
+        hostName: 'timeuntilthething.com',
+      },
+      publicReadAccess: true,
     })
 
-    new route53.ARecord(this, 'non-www-tutt', {
+    const siteDistribution = new CloudFrontWebDistribution(
+      this,
+      'tutt-cloudfront-distribution',
+      {
+        originConfigs: [
+          {
+            behaviors: [{ isDefaultBehavior: true }],
+            s3OriginSource: {
+              s3BucketSource: siteBucket,
+            },
+          },
+        ],
+        aliasConfiguration: {
+          names: ['timeuntilthething.com'],
+          acmCertRef:
+            'arn:aws:acm:us-east-1:185124521435:certificate/cd6946bc-cf42-42f1-b771-c1513fe37bcd',
+        },
+        errorConfigurations: [
+          {
+            errorCode: 403,
+            responseCode: 200,
+            responsePagePath: '/index.html',
+          },
+          {
+            errorCode: 404,
+            responseCode: 200,
+            responsePagePath: '/index.html',
+          },
+        ],
+      }
+    )
+
+    new ARecord(this, 'non-www-tutt', {
       zone,
       recordName: 'timeuntilthething.com',
-      target: route53.RecordTarget.fromAlias(
-        new targets.BucketWebsiteTarget(siteBucket)
-      )
+      target: RecordTarget.fromAlias(
+        new targets.CloudFrontTarget(siteDistribution)
+      ),
     })
 
-    new route53.ARecord(this, 'www-tutt', {
-      zone,
+    new CnameRecord(this, 'www-tutt', {
       recordName: 'www.timeuntilthething.com',
-      target: route53.RecordTarget.fromAlias(
-        new targets.BucketWebsiteTarget(wwwBucket)
-      )
+      domainName: 'timeuntilthething.com',
+      zone: zone,
     })
   }
 }
