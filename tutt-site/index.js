a939c6bb383fd0d3660fc524564caef0d03d2362
Matthew Johnson <matthewjohnson@a-xrc7jjg5j.carfax.cfx>
2021-03-19 23:35:41 -0500

    clock functionality and styles

--- tutt-site/index.js
+++ tutt-site/index.js
@@ -1,1 +1,82 @@
-console.log('Hello World!')
+const vars = {
+  timeinterval: null,
+  endTime: null,
+}
+
+const eles = {
+  content: document.getElementById('content'),
+}
+
+const funcs = {
+  getTimeOfTheThing: () => {
+    fetch('https://api.timeuntilthething.com')
+      .then((response) => response.json())
+      .then(({ date }) => {
+        vars.endTime = date
+        funcs.startCountdown(date)
+      })
+  },
+  getTimeRemaining: (endTime) => {
+    const total = Date.parse(endTime) - Date.parse(new Date())
+
+    return {
+      total: Date.parse(endTime) - Date.parse(new Date()),
+      days: Math.floor(total / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((total / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((total / 1000 / 60) % 60),
+      seconds: Math.floor((total / 1000) % 60),
+    }
+  },
+  startCountdown: (date) => {
+    const timeRemaining = funcs.getTimeRemaining(date)
+
+    eles.content.innerHTML = tmpls.clock(timeRemaining)
+
+    eles.clock = document.getElementById('clock')
+    eles.days = clock.querySelector('.days-container')
+    eles.hours = clock.querySelector('.hours-container')
+    eles.minutes = clock.querySelector('.minutes-container')
+    eles.seconds = clock.querySelector('.seconds-container')
+
+    funcs.updateClock()
+    vars.timeinterval = setInterval(funcs.updateClock, 1000)
+  },
+  updateClock: () => {
+    const { total, days, hours, minutes, seconds } = funcs.getTimeRemaining(
+      vars.endTime
+    )
+
+    eles.days.innerHTML = ('0' + days).slice(-2)
+    eles.hours.innerHTML = ('0' + hours).slice(-2)
+    eles.minutes.innerHTML = ('0' + minutes).slice(-2)
+    eles.seconds.innerHTML = ('0' + seconds).slice(-2)
+
+    if (total <= 0) {
+      clearInterval(vars.timeinterval)
+    }
+  },
+}
+
+const tmpls = {
+  clock: ({ days, hours, minutes, seconds }) => `
+        <div id="clock">
+            <div class="days">
+                <div class="days-container">${days}</div>
+                Days
+            </div>
+            <div class="hours">
+                <div class="hours-container">${hours}</div>
+                Hours
+            </div>
+            <div class="minutes">
+                <div class="minutes-container">${minutes}</div>
+                Minutes
+            </div>
+            <div class="seconds">
+                <div class="seconds-container">${seconds}</div>
+                Seconds
+            </div>
+        </div>
+    `,
+}
+funcs.getTimeOfTheThing()
--- tutt-site/webpack.config.js
+++ tutt-site/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { ProvidePlugin } = require('webpack')
 
 module.exports = {
   mode: 'production',
@@ -11,17 +12,40 @@ module.exports = {
     path: __dirname + '../../tutt-infrastructure/site-files',
     filename: '[name].[chunkhash].js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.ejs$/,
+        loader: 'compile-ejs-loader',
+        options: {
+          htmlmin: true,
+          htmlminOptions: {
+            removeComments: true,
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       inject: 'body',
       chunks: ['index'],
-      template: './template.ejs',
+      template: 'template.ejs',
+      templateParameters: {
+        page: 'index',
+      },
     }),
     new HtmlWebpackPlugin({
       filename: 'app.html',
       inject: 'body',
       chunks: ['app'],
-      template: './template.ejs',
+      template: 'template.ejs',
+      templateParameters: {
+        page: 'app',
+      },
+    }),
+    new ProvidePlugin({
+      _: 'lodash',
     }),
   ],
 }
